93192b046189cbcd2fd1079a1dba2394c7779851
Eyad-dev <eyadahmedhabib2007@gmail.com>
2026-03-23 00:42:50 +0200

    EL 3eed far7a

--- bots/starter/main.py
+++ bots/starter/main.py
@@ -12,7 +12,7 @@ This bot:
 
 import random
 
-from cambc import Controller, Direction, EntityType, Environment, Position
+from cambc import Controller, Direction, EntityType, GameConstants, Environment, Position
 
 # non-centre directions
 DIRECTIONS = [d for d in Direction if d != Direction.CENTRE]
@@ -24,12 +24,19 @@ class Player:
     def run(self, ct: Controller) -> None:
         etype = ct.get_entity_type()
         if etype == EntityType.CORE:
-            if self.num_spawned < 3:
-                # if we haven't spawned 3 builder bots yet, try to spawn one on a random tile
-                spawn_pos = ct.get_position().add(random.choice(DIRECTIONS))
-                if ct.can_spawn(spawn_pos):
-                    ct.spawn_builder(spawn_pos)
-                    self.num_spawned += 1
+
+            rangesquare = GameConstants.CORE_VISION_RADIUS_SQ
+            rangee= (int)(rangesquare**0.5)
+            for x in range(-rangee, rangee + 1):
+                for y in range(-rangee, rangee + 1):
+                    if x**2 + y**2 <= rangesquare:
+                        newposx = ct.get_position().x +x
+                        newposy = ct.get_position().y +y
+                        pos = Position(newposx, newposy)
+                        if ct.get_tile_env(pos) == Environment.ORE_TITANIUM or ct.get_tile_env(pos) ==Environment.ORE_AXIONITE:
+                            print("YEAYAYF_EYFE(FEWIOFHEWOF)")
+                            print("(" + str(newposx) + "," + str(newposy) + ")")
+                            #FIRST IMPLEMENTATION FROM MATIKANETANNHAUSER
         elif etype == EntityType.BUILDER_BOT:
             # if we are adjacent to an ore tile, build a harvester on it
             for d in Direction:
