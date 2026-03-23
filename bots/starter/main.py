d549d727f245ef2cd4ec2951f483e85cd33ee90b
Jasmin Ibrahim <jasminibrahimfathy123@gmail.com>
2026-03-24 01:11:43 +0200

    scanning vision for ores

--- bots/starter/main.py
+++ bots/starter/main.py
@@ -1,18 +1,20 @@
-"""Starter bot - a simple example to demonstrate usage of the Controller API.
-
-Each unit gets its own Player instance; the engine calls run() once per round.
-Use Controller.get_entity_type() to branch on what kind of unit you are.
-
-This bot:
-  - Core: spawns up to 3 builder bots on random adjacent tiles
-  - Builder bot: builds a harvester on any adjacent ore tile, then moves in a
-    random direction (laying a road first so the tile is passable), and places
-    a marker recording the current round number
-"""
-
 import random
 
 from cambc import Controller, Direction, EntityType, GameConstants, Environment, Position
+
+def scan_ore_vision(ct, gameconst):
+    ores=[]
+    rangesquare = gameconst
+    ranged= (int)(rangesquare**0.5)
+    for x in range(-ranged, ranged + 1):
+        for y in range(-ranged, ranged + 1):
+            if x**2 + y**2 <= rangesquare:
+                newposx = ct.get_position().x +x
+                newposy = ct.get_position().y +y
+                pos = Position(newposx, newposy)
+                if ct.get_tile_env(pos) == Environment.ORE_TITANIUM or ct.get_tile_env(pos) ==Environment.ORE_AXIONITE:
+                    ores.append(pos)
+    return ores
 
 # non-centre directions
 DIRECTIONS = [d for d in Direction if d != Direction.CENTRE]
@@ -22,39 +24,35 @@ class Player:
         self.num_spawned = 0 # number of builder bots spawned so far (core)
 
     def run(self, ct: Controller) -> None:
+        
         etype = ct.get_entity_type()
         if etype == EntityType.CORE:
+            ores= scan_ore_vision(ct, GameConstants.CORE_VISION_RADIUS_SQ)
 
-            rangesquare = GameConstants.CORE_VISION_RADIUS_SQ
-            rangee= (int)(rangesquare**0.5)
-            for x in range(-rangee, rangee + 1):
-                for y in range(-rangee, rangee + 1):
-                    if x**2 + y**2 <= rangesquare:
-                        newposx = ct.get_position().x +x
-                        newposy = ct.get_position().y +y
-                        pos = Position(newposx, newposy)
-                        if ct.get_tile_env(pos) == Environment.ORE_TITANIUM or ct.get_tile_env(pos) ==Environment.ORE_AXIONITE:
-                            print("YEAYAYF_EYFE(FEWIOFHEWOF)")
-                            print("(" + str(newposx) + "," + str(newposy) + ")")
-                            #FIRST IMPLEMENTATION FROM MATIKANETANNHAUSER
         elif etype == EntityType.BUILDER_BOT:
-            # if we are adjacent to an ore tile, build a harvester on it
-            for d in Direction:
-                check_pos = ct.get_position().add(d)
-                if ct.can_build_harvester(check_pos):
-                    ct.build_harvester(check_pos)
-                    break
-            
-            # move in a random direction
-            move_dir = random.choice(DIRECTIONS)
-            move_pos = ct.get_position().add(move_dir)
-            # we need to place a conveyor or road to stand on, before we can move onto a tile
-            if ct.can_build_road(move_pos):
-                ct.build_road(move_pos)
-            if ct.can_move(move_dir):
-                ct.move(move_dir)
+            # Move towards a target
+            direction = ct.get_position().direction_to(ores)
+            if ct.can_move(direction):
+                ct.move(direction)
 
-            # place a marker on an adjacent tile with the current round number
-            marker_pos = ct.get_position().add(random.choice(DIRECTIONS))
-            if ct.can_place_marker(marker_pos):
-                ct.place_marker(marker_pos, ct.get_current_round())
+
+            # # if we are adjacent to an ore tile, build a harvester on it
+            # for d in Direction:
+            #     check_pos = ct.get_position().add(d)
+            #     if ct.can_build_harvester(check_pos):
+            #         ct.build_harvester(check_pos)
+            #         break
+            
+            # # move in a random direction
+            # move_dir = random.choice(DIRECTIONS)
+            # move_pos = ct.get_position().add(move_dir)
+            # # we need to place a conveyor or road to stand on, before we can move onto a tile
+            # if ct.can_build_road(move_pos):
+            #     ct.build_road(move_pos)
+            # if ct.can_move(move_dir):
+            #     ct.move(move_dir)
+
+            # # place a marker on an adjacent tile with the current round number
+            # marker_pos = ct.get_position().add(random.choice(DIRECTIONS))
+            # if ct.can_place_marker(marker_pos):
+            #     ct.place_marker(marker_pos, ct.get_current_round())
