0926cc2784f923568ae639be3d8c61cbdbf7d82a
Jasmin Ibrahim <jasminibrahimfathy123@gmail.com>
2026-03-24 03:14:31 +0200

    discovering functions wow

--- bots/starter/scanning.py
+++ bots/starter/scanning.py
@@ -1,16 +1,10 @@
 from cambc import Controller, Direction, EntityType, GameConstants, Environment, Position
 
 def scan_ore_vision(ct, gameconst):
+    tiles= ct.get_nearby_tiles() 
     ores=[]
-    rangesquare = gameconst
-    ranged= (int)(rangesquare**0.5)
-    for x in range(-ranged, ranged + 1):
-        for y in range(-ranged, ranged + 1):
-            if x**2 + y**2 <= rangesquare:
-                newposx = ct.get_position().x +x
-                newposy = ct.get_position().y +y
-                pos = Position(newposx, newposy)
-                if ct.get_tile_env(pos) == Environment.ORE_TITANIUM or ct.get_tile_env(pos) ==Environment.ORE_AXIONITE:
-                    ores.append(pos)
+    for tile in tiles:
+        if ct.get_tile_env(tile) == Environment.ORE_TITANIUM or ct.get_tile_env(tile) ==Environment.ORE_AXIONITE:
+                    ores.append(tile)
     return ores
 
