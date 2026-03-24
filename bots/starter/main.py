bd11d9d11d73c47d7bc157b82a2f30bbbf1073e3
Eyad-dev <eyadahmedhabib2007@gmail.com>
2026-03-24 02:17:27 +0200

    dumb ahh bot movement

--- bots/starter/main.py
+++ bots/starter/main.py
@@ -22,20 +22,47 @@ DIRECTIONS = [d for d in Direction if d != Direction.CENTRE]
 class Player:
     def __init__(self):
         self.num_spawned = 0 # number of builder bots spawned so far (core)
-
+        self.heading = random.choice(DIRECTIONS)
     def run(self, ct: Controller) -> None:
-        
         etype = ct.get_entity_type()
         if etype == EntityType.CORE:
+            if self.num_spawned <1:
+                spawn_pos = ct.get_position().add(random.choice(DIRECTIONS))
+                if ct.can_spawn(spawn_pos):
+                    ct.spawn_builder(spawn_pos)
+                self.num_spawned += 1
             ores= scan_ore_vision(ct, GameConstants.CORE_VISION_RADIUS_SQ)
+            print(ores)
+            
 
+
+            #40x40 grid
+            #Core A (11, 25)
+            #Core B (28,14)
+            #Core B ((40-11)-1, (40-25)-1)
+            #Opponent core equals
+            #Core ((grid_length - CoreAx)-1 , (grid_height - CoreAy) -1)
         elif etype == EntityType.BUILDER_BOT:
             # Move towards a target
-            direction = ct.get_position().direction_to(ores)
-            if ct.can_move(direction):
-                ct.move(direction)
+            # direction = ct.get_position().direction_to(ores)
+            # if ct.can_move(direction):
+            #     ct.move(direction)
+            
+            ores = scan_ore_vision(ct, GameConstants.BUILDER_BOT_VISION_RADIUS_SQ)
 
-
+            if(ores):
+                for i in ores:
+                    print(i)
+            move_pos = ct.get_position().add(self.heading)
+            if ct.can_build_road(move_pos):
+                ct.build_road(move_pos)
+                
+            # Try to move forward
+            if ct.can_move(self.heading):
+                ct.move(self.heading)
+            
+            else:
+                self.heading = random.choice(DIRECTIONS)
             # # if we are adjacent to an ore tile, build a harvester on it
             # for d in Direction:
             #     check_pos = ct.get_position().add(d)
