2df7c72108eeaf5f324a882380886c63d6a93f9e
Yahia Ahmed <yahiaahmed2yahia@hotmail.com>
2026-04-13 19:58:46 +0200

    headpatters employment

--- bots/starter/healer.py
+++ bots/starter/healer.py
@@ -4,7 +4,14 @@ from scanning import *
 from snipe import *
 
 def healerrun(self, ct: Controller):
-    if ct.can_heal(self.ourcoord):
-        ct.heal(self.ourcoord)
-
-    ct.get_nearby_entities()
+    nearby_entities = ct.get_nearby_entities()
+    for entity in nearby_entities:
+        if ct.get_type(entity) == EntityType.BUILDER_BOT and ct.get_team(entity) == self.our_team:
+            if ct.get_hp(entity) < ct.get_max_hp(entity):
+                if ct.can_heal(ct.get_position(entity)):
+                    ct.heal(ct.get_position(entity))
+        if ct.get_type(entity) == EntityType.CORE and ct.get_team(entity) == self.our_team:
+            if ct.get_hp(entity) < ct.get_max_hp(entity):
+                if ct.can_heal(ct.get_position(entity)):
+                    ct.heal(ct.get_position(entity))
+    
