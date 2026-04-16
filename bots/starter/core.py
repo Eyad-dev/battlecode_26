e443bcb7b599f054d8c0916c89fdad63f2536d56
Eyad-dev <eyadahmedhabib2007@gmail.com>
2026-04-16 02:03:19 +0200

    sweetya

--- bots/starter/core.py
+++ bots/starter/core.py
@@ -12,8 +12,6 @@ DIAGONAL_DIRS = [
 
 def corerrun(self, ct: Controller):
     self.turn_counter += 1
-    test_pos2 = None
-    test_pos3 = None
     spawn_pos = ct.get_position().add(random.choice(DIRECTIONS))
     # for d in DIAGONAL_DIRS:
     #     if (self.axioniter_spawned == False and self.turn_counter >= 1500):
@@ -26,26 +24,34 @@ def corerrun(self, ct: Controller):
     #             self.num_spawned += 1
     #             break
    
-    if self.num_spawned < 5:
-            if self.num_spawned <= 2:
-                 role_id = 1 # HARVESTER
-            elif (ct.get_hp() < ct.get_max_hp() and healers_spawned < 5):
-                role_id = 3 # HEALER
-                if ct.can_spawn(spawn_pos):
-                    ct.spawn_builder(spawn_pos)
-                    healers_spawned += 1                 
+    if self.num_spawned < 2:
+            if self.num_spawned <= 1:
+                 role_id = 4 # AXIONITER
+            elif self.num_spawned <= 2:
+                 role_id = 1 # HARVESTER               
             else:
                  role_id = 2 # ATTACKER
             
-            writemarker(self,ct,role_id)
-
             if ct.can_spawn(spawn_pos):
                 ct.spawn_builder(spawn_pos)
                 self.num_spawned += 1
     
+            writemarker(self,ct,role_id)
+    
+    if (ct.get_hp() < ct.get_max_hp() and self.healers_spawned < 5):
+        role_id = 3 # HEALER
+        for d in DIRECTIONS:
+            test_pos = ct.get_position().add(d)
+            if ct.can_spawn(test_pos):
+                ct.spawn_builder(test_pos)
+                self.healers_spawned += 1
+                writemarker(self,ct,role_id)
+                break
+            
+            
 
-        
-         
+
+    
 
 def writemarker(self,ct,role_id):
     # Place marker on a diagonal to avoid splitter positions
